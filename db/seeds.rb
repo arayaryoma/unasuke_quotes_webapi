cb42eeaefc779a50c220d7f54922b82747ae96cc
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-03-27 01:21:25 +0900

    Insert initialize unasuke tweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'CSV'
+tweets = CSV.table('/Users/Ryoma/Downloads/108252390_63123566238ddc7bdc1229c0a4b0d4847bdefb75/tweets.csv')
+#tweets = CSV.table('/Users/Ryoma/allajah_hack/terakoya/unasuke_quotes/testfile.csv')
+tweets.each do |tweet|
+  if tweet[:in_reply_to_status_id].blank? && tweet[:retweeted_status_id].blank?
+    Unasuke.create!({tweet_id: tweet[:tweet_id], tweet_time_stamp: DateTime.parse(tweet[:timestamp]), tweet_text: tweet[:text]})
+  end
+end
