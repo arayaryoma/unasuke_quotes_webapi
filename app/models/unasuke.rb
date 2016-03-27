7d5bcbc6a4773a61449d3900ba153811ed7cb6be
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-03-27 19:17:47 +0900

    Create unasuke model

---
+++ app/models/unasuke.rb
@@ -0,0 +1,10 @@
+class Unasuke < ApplicationRecord
+  def client
+    Twitter::REST::Client.new do |config|
+      config.consumer_key = Rails.application.secrets.consumer_key
+      config.consumer_secret = Rails.application.secrets.consumer_secret
+      config.access_token = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
+    end
+  end
+end
