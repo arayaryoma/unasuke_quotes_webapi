f1e076f01a7388866998864bf6afcb459ec35913
Ryoma Abe <ryoma.abe@fuller.co.jp>
2016-03-27 19:42:27 +0900

    Change database to postgresql from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160326070902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "unasukes", force: :cascade do |t|
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
